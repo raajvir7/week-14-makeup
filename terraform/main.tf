381ec243ca4bbabddfb54bb08d0c3e96fdb1377a
raajvir7 <raajvirrandhawa2311@gmail.com>
2025-12-02 15:44:04 -0800

    t

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,16 @@ data "aws_ami" "debian" {
   }
 }
 
+data "aws_ami" "rocky" {
+  most_recent = true
+  owners      = ["792107900819"] 
+
+  filter {
+    name   = "name"
+    values = ["Rocky-9-ec2-*-x86_64-*"]
+  }
+}
+
 # Create a VPC
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc
 resource "aws_vpc" "web" {
@@ -124,7 +134,7 @@ module "database" {
   project_name           = local.project_name # project name from local
   ec2_name               = "database"
   ec2_role               = "Database"
-  ami                    = "ami-093bd987f8e53e1f2"
+  ami = data.aws_ami.rocky.id
   key_name               = "aws-4640"                  # SSH key name
   vpc_security_group_ids = [aws_security_group.web.id] # Pass security group IDs here
   subnet_id              = aws_subnet.web.id           # Pass the subnet ID here
